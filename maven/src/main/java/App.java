9078e19c459021cea4a27c5c8e5987d0e163d71e
wanhui2016 <wanhui95@163.com>
2016-12-12 18:17:45 +0800

    2.0

--- maven/src/main/java/App.java
+++ maven/src/main/java/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("wanhui");
+        System.out.println("wanhui2.0");
     }
 }
